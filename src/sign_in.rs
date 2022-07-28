ae110aa31d45d709ac804926face64b3440acea1
lucus.eth <lucusra@protonmail.com>
2022-07-29 00:17:26 +1000

    add: dynamic confirmation

--- src/sign_in.rs
+++ src/sign_in.rs
@@ -1,6 +1,5 @@
 use std::str::FromStr;
-
-use ethers::{prelude::{*, k256::ecdsa::SigningKey}, utils::keccak256};
+use ethers::{prelude::{*, k256::ecdsa::SigningKey}};
 
 pub async fn setup_signer(
     priv_key: String,
@@ -20,14 +19,18 @@ pub async fn setup_signer(
         }
     }
 }
-    
-
-// 0xe5a12cfa871dfa50c8085ec25356a984d241b13ac0db1f5b8185ffdfb8fd8c63 7f9c5ff25287b33bc9992ba04ed84d1b2c180e724d487eda85f08e18a06c9af6 1c
-// 0x9d0a74e1c6cb58cc8f5695fb0c1b7f2fc9795c47c45e35f05919555b2f90fa65 5cde29bba787dcb5e544c3b0604ba4b82a71178dde9e5ac04f3f1db151d0a194 1b
-pub fn confirm_pub_key(msg: &str) -> Option<H160> {
-    let x: Signature = FromStr::from_str("0x9d0a74e1c6cb58cc8f5695fb0c1b7f2fc9795c47c45e35f05919555b2f90fa655cde29bba787dcb5e544c3b0604ba4b82a71178dde9e5ac04f3f1db151d0a1941b").unwrap();
-    println!("{:?}", x);
-    let r = x.recover(msg);
+   
+/// Example:
+/// 
+/// The signature: 
+/// 0x9d0a74e1c6cb58cc8f5695fb0c1b7f2fc9795c47c45e35f05919555b2f90fa655cde29bba787dcb5e544c3b0604ba4b82a71178dde9e5ac04f3f1db151d0a1941b
+/// 
+/// Gives the signer: 
+/// 0xdcd49c36e69bf85fa9c5a25dea9455602c0b289e
+pub fn confirm_pub_key(signature: &str, msg: &str) -> Option<H160> {
+    let sig: Signature = FromStr::from_str(signature).unwrap();
+    println!("{:?}", signature);
+    let r: Result<H160, SignatureError> = sig.recover(msg);
     match r {
         Ok(signer) => return Some(signer),
         Err(_) => return None,
